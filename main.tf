b16ae94e7c85a05cc496de8c141eb99f6ddb98a9
sumitmoral <146178785+sumitmoral@users.noreply.github.com>
2023-10-23 10:30:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "aws_subnet" "subnet" {
   vpc_id                  = var.vpc_id
   tags = merge(
     {
-      Name = format("%s-%d", var.subnet_name,count.index+1)
+      Name = format("%s-%d", var.subnet_name)
     },
     var.tags,
   )
